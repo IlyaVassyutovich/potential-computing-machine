204188aec14259b3fc25c6577be408e97d2a2cba
Ilya Vassyutovich <me@iv.link>
2021-01-09 18:10:01 +0300

    Implement logging to file

--- src/Worker/Program.cs
+++ src/Worker/Program.cs
@@ -1,9 +1,9 @@
+using System;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.EventLog;
 
 namespace IV.PCM.Worker
 {
@@ -21,11 +21,31 @@ namespace IV.PCM.Worker
 		{
 			return Host
 				.CreateDefaultBuilder(args)
-				// TODO: Implement custom source for event log
-				.ConfigureLogging(loggingBuilder => loggingBuilder.AddEventLog())
+				.ConfigureLogging(ConfigureLogging)
 				.ConfigureWebHostDefaults(whb => whb.UseStartup<Startup>())
-				.ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); })
+				.ConfigureServices((_, services) => services.AddHostedService<Worker>())
 				.UseWindowsService();
+		}
+
+		// TODO: Move part of logging configuration to Startup (aka web-api only)?
+		private static void ConfigureLogging(ILoggingBuilder loggingBuilder)
+		{
+			var configuringDateTimeUtc = DateTime.UtcNow;
+			// TODO: Remove hardcoded path
+			var logFileName = string.Format(
+				@"c:\Program Files\potential-computing-machine\logs\pcm_{0}.log",
+				configuringDateTimeUtc.ToString("s").Replace(":", "-"));
+			
+			loggingBuilder
+				// TODO: Implement custom source for event log
+				.AddEventLog(eventLogSettings => eventLogSettings.Filter = (_, logLevel) => logLevel >= LogLevel.Warning)
+				.AddFile(
+					logFileName,
+					fileLoggerOptions =>
+					{
+						fileLoggerOptions.MaxRollingFiles = 5;
+						fileLoggerOptions.Append = true;
+					});
 		}
 	}
 }
